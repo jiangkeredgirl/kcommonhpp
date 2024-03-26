556eb27963dca1197c61e46a0308640c6af8e124
jiangke <jiangkeredgirl@hotmail.com>
2024-03-26 10:10:06 +0800

    增加一个函数判断gbk字符串

--- CodeFormat.h
+++ CodeFormat.h
@@ -118,6 +118,43 @@ inline bool is_str_utf8(const char* str)
 	return true;
 }
 
+bool is_str_gbk(const char* str)
+{
+	unsigned int nBytes = 0;//GBK可用1-2个字节编码,中文两个 ,英文一个
+	unsigned char chr = *str;
+	bool bAllAscii = true; //如果全部都是ASCII,
+	for (unsigned int i = 0; str[i] != '\0'; ++i) {
+		chr = *(str + i);
+		if ((chr & 0x80) != 0 && nBytes == 0) {// 判断是否ASCII编码,如果不是,说明有可能是GBK
+			bAllAscii = false;
+		}
+		if (nBytes == 0) {
+			if (chr >= 0x80) {
+				if (chr >= 0x81 && chr <= 0xFE) {
+					nBytes = +2;
+				}
+				else {
+					return false;
+				}
+				nBytes--;
+			}
+		}
+		else {
+			if (chr < 0x40 || chr>0xFE) {
+				return false;
+			}
+			nBytes--;
+		}//else end
+	}
+	if (nBytes != 0) {   //违返规则
+		return false;
+	}
+	if (bAllAscii) { //如果全部都是ASCII, 也是GBK
+		return true;
+	}
+	return true;
+}
+
 inline std::string /*gb2312_to_*/utf8(std::string const& strGb2312)
 {
 	std::vector<wchar_t> buff(strGb2312.size());
